e20df0dc1ee59491fa99068402f4aade7771fdd2
Uwana Ikaiddi <uwikaiddi@gmail.com>
2021-05-14 08:20:21 -0500

    Add in-code documentation

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * An array of "quote" objects.
+ * @property {string} quote -  A line of text or speech.
+ * @property {string} source -  Who produced the quote.  
+ * @property {string} citation -  Where the quote was written/spoken.
+ * @property {number} year -  The year the quote was produced.
+ * @property {string} tag -  The category associated with the quote.
 ***/
 const quotes = [
   {
@@ -17,76 +19,76 @@ const quotes = [
     source: "Oscar Wilde",
     citation: "A Few Maxims for the Instruction of the Over-Educated",
     year: 1894,
-    tags: "Education"
+    tag: "Education"
   },
   {
     quote: "Art is the only serious thing in the world. And the artist is the only person who is never serious.",
     source: "Oscar Wilde",
     citation: "A Few Maxims for the Instruction of the Over-Educated",
     year: 1894,
-    tags: "Art"
+    tag: "Art"
   },
   {
     quote: "Experience is the name everyone gives to their mistakes.",
     source: "Oscar Wilde",
     citation: "The Nihilists",
     year: 1883,
-    tags: "Experience"
+    tag: "Experience"
   },
   {
     quote: "Most people are other people. Their thoughts are someone else's opinions, their lives a mimicry, their passions a quotation.",
     source: "Oscar Wilde",
     citation: "De Profundis",
     year: 1897,
-    tags: "People"
+    tag: "People"
   },
   {
     quote: "It is absurd to divide people into good and bad. People are either charming or tedious.",
     source: "Oscar Wilde",
     citation: "Lady Windermere's Fan",
     year: 1892,
-    tags: "People"
+    tag: "People"
   },
   {
     quote: "Life imitates art far more than art imitates Life.",
     source: "Oscar Wilde",
     citation: "The Decay of Lying",
     year: 1889,
-    tags: "Life"
+    tag: "Life"
   },
   {
     quote: "Anybody can make history. Only a great man can write it.",
     source: "Oscar Wilde",
     citation: "The Critic as Artist",
     year: 1891,
-    tags: "History"
+    tag: "History"
   },
   {
     quote: "To live is the rarest thing in the world. Most people exist, that is all.",
     source: "Oscar Wilde",
     citation: "The Soul of Man Under Socialism",
     year: 1891,
-    tags: "Life"
+    tag: "Life"
   },
   {
     quote: "Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth.",
     source: "Oscar Wilde",
     citation: "The Critic as Artist",
     year: 1891,
-    tags: "Truth"
+    tag: "Truth"
   },
   {
     quote: "One should always be a little improbable.",
     source: "Oscar Wilde",
     citation: "Phrases and Philosophies for the Use of the Young",
     year: 1894,
-    tags: "Self"
+    tag: "Self"
   }
 ];
 
 
 /***
- * `getRandomQuote` function
+ * Randomly selects and returns a quote object from the quotes array.
 ***/
 function getRandomQuote() {
   let randomNumber = (Math.floor(Math.random() * quotes.length - 1) + 1);
@@ -94,7 +96,8 @@ function getRandomQuote() {
 };
 
 /***
- * `printQuote` function
+ * Returns an HTML string that contains specific formatting for 
+ * the quote randomly selected by the getRandomQuote function.
 ***/
 function printQuote() {
   let randomQuote = getRandomQuote();
@@ -108,15 +111,17 @@ function printQuote() {
   if (randomQuote.year) {
     htmlString += `<span class="year">${randomQuote.year}</span>`;
   };
-  if (randomQuote.tags) {
-    htmlString += `<br><br><p><strong>Tags:</strong> ${randomQuote.tags}</p>`;
+  if (randomQuote.tag) {
+    htmlString += `<br><br><p><strong>Tags:</strong> ${randomQuote.tag}</p>`;
   };
   htmlString += `</p>`;
   return htmlString;
 };
 
 /***
- * `randomizeColor` function
+ * Returns a randomized color in the rgb() syntax.
+ * Note: Each number in the rgb() synax only goes up to 250 
+ * to prevent too light of a background for viewing the quote.
 ***/
 function randomizeColor() {
   let red = Math.floor(Math.random() * 250);
@@ -126,7 +131,11 @@ function randomizeColor() {
   return backgroundColor;
 };
 
-
+/***
+ * This setInterval function contains the following commands to repeat every 10 seconds:
+ * 1. Add the printQuote function return to the HTML ID "quote-box".
+ * 2. Randomize the color of the page background.
+ ***/
 setInterval(function() {
   document.getElementById('quote-box').innerHTML = printQuote();
   document.body.style.background = randomizeColor();
